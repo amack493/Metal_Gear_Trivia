0fedd9cb33f475a137ea6a7da29107e1d2cce406
Tre <amack4693@gmail.com>
2019-10-22 11:16:05 -0400

    added test question to js

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ easy.addEventListener('click', function (e) {
     console.log('working ;)')
 })
 
+const questions = [ 
+    {prompt: 'Who is the main character of Metal Gear Solid\n(a) Solid Snake\n\(b) Psycho Mantis\n\(c) Revolver Ocelot',
+answer: "a"}
+]
+
+let score = 0;
+
+for(let i = 0; i < questions.length; i++){
+    const response = window.prompt(questions[i].prompt);
+    if(response == questions[i].answer){
+        score++;
+        alert("CORRECT!");
+    } else{
+        alert("INCORRECT!");
+    }
+}
