fb15ee4589525a070afbb99e2e14a23a0a2c95bb
Tre <amack4693@gmail.com>
2019-10-22 16:42:25 -0400

    changed classes in index.html

--- script.js
+++ script.js
@@ -6,19 +6,80 @@ easy.addEventListener('click', function (e) {
     console.log('working ;)')
 })
 
-const questions = [ 
-    {prompt: 'Who is the main character of Metal Gear Solid\n(a) Solid Snake\n\(b) Psycho Mantis\n\(c) Revolver Ocelot',
-answer: "a"}
-]
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
 
-let score = 0;
+function buildQuiz(){}
 
-for(let i = 0; i < questions.length; i++){
-    const response = window.prompt(questions[i].prompt);
-    if(response == questions[i].answer){
-        score++;
-        alert("CORRECT!");
-    } else{
-        alert("INCORRECT!");
-    }
+function showResults(){}
+
+//builds quiz
+buildQuiz();
+//stores html output
+const output = [];
+
+// questions.forEach(
+//     currentQuestion, questionNumber); {
+//         //stores answers
+//         const answers = []
+
+//         //for each available answer
+//         for(letter in currentQuestion.answers){
+//             answers.push
+            
+//         }
+//     }
+
+
+//when submitted shows results
+// submitButton.addEventListeners('click', showResults);
+
+const questions = [
+{
+    question: "Who's the main character in Metal Gear Solid?",
+    answers: {
+        a: 'Solid Snake',
+        b: 'Psycho Mantis',
+        c: 'Revolver Ocelot'
+    },
+    correctAnswer: 'a'
+},
+{
+    question: 'Who created Metal Gear?',
+    answers: {
+        a: 'Genndy Tartakovsky',
+        b: 'Hideo Kojima',
+        c: 'Stephen Hillenberg'
+    },
+    correctAnswer: 'b'
+},
+{
+    question: "What's the best disguise?",
+    answers: {
+        a: 'A cardboard Box',
+        b: 'An Enemy Uniform',
+        c: 'A fake mustache'
+    },
+    correctAnswer: 'a'
 }
+];
+
+// const questions = [ 
+//     {prompt: 'Who is the main character of Metal Gear Solid\n(a) Solid Snake\n\(b) Psycho Mantis\n\(c) Revolver Ocelot',
+// answer: "a"},
+// {prompt: 'Who created Metal Gear?\n(a)Gendy Tartokovsky\n\(b)Hideo Kojima\n\(c)Steven Hillenberg',
+// answer: "b"}
+// ]
+
+//  score = 0;
+
+// for(let i = 0; i < questions.length; i++){
+//     const response = window.prompt(questions[i].prompt);
+//     if(response == questions[i].answer){
+//         score++;
+//         alert("CORRECT!");
+//     } else{
+//         alert("INCORRECT!");
+//     }
+// }
